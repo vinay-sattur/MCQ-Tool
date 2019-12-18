291ca44d237e8ba7715366595be0378d7a686e62
Vaishnavi M <vaishnavi.m@Triconinfotech.com>
2019-12-18 19:28:02 +0530

    Added Ngx-shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgxSharedModule, TranslateModule } from '@mhe/ngx-shared';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxSharedModule,
+    TranslateModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
